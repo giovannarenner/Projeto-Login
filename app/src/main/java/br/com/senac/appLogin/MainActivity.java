c8c5a29d2f3e25c9ec537987ec1ce82b4a152f63
GiovannaRenner <giovannarenner2@gmail.com>
2022-10-19 20:24:59 -0300

    Projeto Login

---
+++ app/src/main/java/br/com/senac/appLogin/MainActivity.java
@@ -0,0 +1,41 @@
+package br.com.senac.appLogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+        public class MainActivity extends AppCompatActivity {
+            Button btEntrar, btRegistar;
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_login);
+                btEntrar =(Button) findViewById(R.id.btEntrar);
+                btRegistar =(Button) findViewById(R.id.btRegistrar);
+
+                btEntrar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                        startActivity(i);
+
+                    }
+                });
+
+                btRegistar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent i = new Intent(MainActivity.this, RegistrarActivity.class);
+                        startActivity(i);
+
+                    }
+                });
+
+
+            }
+        }
+
+
